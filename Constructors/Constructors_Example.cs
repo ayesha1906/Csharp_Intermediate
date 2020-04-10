2b81ae6a48b763b0bf4bdc76d5c8c89af511ce7e
ayesha1906 <memonayesha458@gmail.com>
2020-04-10 20:23:31 +0530

    Constructors

---
+++ Constructors/Constructors_Example.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Order
+    {
+
+    }
+   
+    class Constructors_Example
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1, "Ayesha");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+            Console.WriteLine();
+
+            var cust = new Customer(1);
+            Console.WriteLine(cust.Id);
+            Console.WriteLine(cust.Name);
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
+
+        }
+    }
+}
---
+++ Constructors/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructors
+{
+    class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id)
+            :this()               
+            /* using this() it will pass the contents of the above const i.e we do not need to initialize 
+             * the Orders List again in this constructor. This will reduce repetitions of lines*/
+
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            :this(id)
+        {
+           // this.Id = id;        because calling the above const with id parameter using this ()
+            this.Name = name;
+        }
+    }
+}
