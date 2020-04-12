a507822e39f16ecb0c4e5669b6ff6b8209bffbe2
ayesha1906 <memonayesha458@gmail.com>
2020-04-12 20:22:54 +0530

    Properties_get_set

---
+++ Properties/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public Person(DateTime birthdate)
+        {
+            Birthhdate = birthdate;
+        }
+        public DateTime Birthhdate { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthhdate;
+                var years = timeSpan.Days / 365;
+                return years;
+            }
+        }
+    }
+}
