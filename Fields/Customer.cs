724f18289dab6148c35c60791132544e82b28def
ayesha1906 <memonayesha458@gmail.com>
2020-04-12 12:06:40 +0530

    Read-only Fields

---
+++ Fields/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public  List<Order> Orders = new List<Order>();
+      //  public readonly List<Order> Orders = new List<Order>();
+
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = Name;
+        }
+
+        public void Promote()
+        {
+            Orders = new List<Order>();
+
+        }
+    }
+}
---
+++ Fields/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine(customer.Orders.Count);  //display 2
+
+            customer.Promote();
+
+            /* now will display 0 since due to calling promote method our list is initialized again .
+             * So all of the data is lost.     
+             * Thus we can use readonly  modifier to make sure that we can initialise our field only once
+             * */
+            Console.WriteLine(customer.Orders.Count); //0
+
+
+        }
+    }
+}
