319864752a2b175b854166ed22134033749c74a6
ayesha1906 <memonayesha458@gmail.com>
2020-04-12 12:08:47 +0530

    Access Modifiers example

---
+++ Access_Modifiers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+    class Access_Modifiers
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1998, 1, 7));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
