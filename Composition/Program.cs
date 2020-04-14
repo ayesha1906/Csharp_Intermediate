88aa3cce37b353d54f39b8243c71b69523db19a8
ayesha1906 <memonayesha458@gmail.com>
2020-04-14 23:32:48 +0530

    Basics of Composition

---
+++ Composition/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Composition
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            installer.Install();
+
+        }
+    }
+}
