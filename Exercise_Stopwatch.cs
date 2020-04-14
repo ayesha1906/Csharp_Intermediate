04419fcfd2698a6ccd052c4a54e2e7defdd6b003
ayesha1906 <memonayesha458@gmail.com>
2020-04-14 19:10:17 +0530

    Stopwatch exercise

---
+++ Exercise_Stopwatch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace Exercise_1
+{
+
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        private bool _running;
+
+        public void Start()
+        {
+
+            if(_running)
+                throw new InvalidOperationException("Stopwatch is already Running");
+               
+            
+                _startTime = DateTime.Now;
+                _running = true;
+            
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+                throw new InvalidOperationException("Stopwatch is not Running");
+
+            
+                _endTime = DateTime.Now;
+                _running = false;
+            
+        }
+
+        public TimeSpan GetInterval()
+        {
+           
+             return _endTime - _startTime;
+            
+        }
+    }
+    class Exercise_Stopwatch
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new StopWatch();
+
+            for(var i=0;i<2;i++)
+            {
+                stopwatch.Start();
+
+                Thread.Sleep(1000);
+
+                stopwatch.Stop();
+
+                Console.WriteLine("Duration: "+stopwatch.GetInterval());
+                Console.WriteLine("Press enter to run the stopwatch one more time .");
+                Console.ReadLine();
+            }
+        }
+    }
+}
