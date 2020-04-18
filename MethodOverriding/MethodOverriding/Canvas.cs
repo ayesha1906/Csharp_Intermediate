143b596022f2039f8f7981452e3659bd235efa03
ayesha1906 <memonayesha458@gmail.com>
2020-04-18 16:00:33 +0530

    Overriding

---
+++ MethodOverriding/MethodOverriding/Canvas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MethodOverriding
+{
+   
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach(var shape in shapes)
+            {
+                shape.Draw();   
+            }
+        }
+    }
+}
