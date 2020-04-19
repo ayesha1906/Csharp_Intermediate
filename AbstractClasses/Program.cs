ccfcc72e2bab069114599ac3811add4b1e5440bd
ayesha1906 <memonayesha458@gmail.com>
2020-04-19 11:49:17 +0530

    Abstract Class

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var shape = new Shape(); //error since it is abstract
+
+            var circle = new Circle();
+            circle.Draw();
+
+            var rect = new Rectangle();
+            rect.Draw();
+
+        }
+    }
+}
---
+++ AbstractClasses/Shape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace AbstractClasses
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        /*
+        public  void Draw()
+        {
+         if we keep this method as not abstract then the draw method from rect class would not 
+         do any implementation. Hence for the developer to implement this method we can make it as abstract.
+
+        }
+        */
+
+        public abstract void Draw();
+
+      public void Cope()
+        {
+            Console.WriteLine("Copied Shape into clipboard");
+        }
+
+        public void Select()
+        {
+            Console.WriteLine("Select the Shape.");
+        }
+    }
+}
