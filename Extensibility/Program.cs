f7604c0dc9343de6afb7032a458ace45fdd80528
ayesha1906 <memonayesha458@gmail.com>
2020-04-22 17:52:46 +0530

    Extensibility and inheritance

---
+++ Extensibility/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var dbMigrator = new DbMigrator(new ConsoleLogger());
+            //or through FileLogger
+          var dbMigrator1 = new DbMigrator(new FileLogger(@"D:\VisualStudioCodes\C# Intermediate\Extensibility\Extensibility\log.txt"));
+            
+            dbMigrator.Migrate();
+            dbMigrator1.Migrate(); //creates a log.txt file and shows output in it 
+
+        }
+    }
+}
