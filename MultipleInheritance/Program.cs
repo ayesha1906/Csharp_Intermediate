35ff79ff19bccdb0e688f6b6286bd0f4054ad6d9
ayesha1906 <memonayesha458@gmail.com>
2020-04-22 19:08:30 +0530

    Interface and multipleInheritance

---
+++ MultipleInheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MultipleInheritance
+{
+    public class UiControl
+    {
+        public string Id { get; set; }
+        public Size Size { get; set; }
+        public Position  TopLeft { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Draw Shape");
+        }
+
+        public void Focus()
+        {
+            Console.WriteLine("Received Focus");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var txtbox = new TextBox();
+            txtbox.Draw();
+            txtbox.Drag();
+
+        }
+    }
+}
---
+++ MultipleInheritance/TextBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MultipleInheritance
+{
+    public class TextBox : UiControl, IDraggable, IDroppable
+    {
+        public void Drag()
+        {
+            Console.WriteLine("shape dragged");
+        }
+
+        public void Drop()
+        {
+            Console.WriteLine("Shape dropped.");
+        }
+    }
+}
